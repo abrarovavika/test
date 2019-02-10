a05aca69586c63347d7d3851f5d60678ddef88bf
Marat Abrarov <abrarov@gmail.com>
2019-02-10 23:49:11 +0300

    Added comments

--- src/main/java/ru/abrarovavika/Main.java
+++ src/main/java/ru/abrarovavika/Main.java
@@ -1,9 +1,13 @@
 package ru.abrarovavika;
 
+/**
+ * Entry point
+ */
 public class Main {
 
   public static void main(String[] args) {
     System.out.println("Hello, World!");
+    // Another hello
     System.out.println("Hello, Aida");
   }
 }
