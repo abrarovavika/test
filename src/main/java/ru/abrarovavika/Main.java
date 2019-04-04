945b2048728397418cdc6075d3ef89f897ad72a7
Marat Abrarov <abrarov@gmail.com>
2019-04-04 21:20:06 +0300

    Modulo calculation

--- src/main/java/ru/abrarovavika/Main.java
+++ src/main/java/ru/abrarovavika/Main.java
@@ -1,13 +1,29 @@
 package ru.abrarovavika;
 
+import java.io.*;
+
 /**
  * Entry point
  */
 public class Main {
 
-  public static void main(String[] args) {
-    System.out.println("Hello, World!");
-    // Another hello
-    System.out.println("Hello, Aida");
+  public static void main(String[] args) throws IOException {
+    int x;
+    int y;
+
+    try (InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+         BufferedReader reader = new BufferedReader(inputStreamReader)) {
+      System.out.print("x = ");
+      x = Integer.parseInt(reader.readLine());
+      System.out.print("y = ");
+      y = Integer.parseInt(reader.readLine());
+    }
+
+    if (y != 0) {
+      int modulo = x % y;
+      System.out.println(x + " % " + y + " = " + modulo);
+    } else {
+      System.out.println("y should be non-zero");
+    }
   }
 }
